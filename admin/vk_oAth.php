b6e876f3ea06ed63ecbd0058af1416df3daf7fd3
timurt <borodatimur@gmail.com>
2022-03-07 17:02:04 +0300

    Fix: we take the parameters for initializing the class from the configuration constants

--- admin/vk_oAth.php
+++ admin/vk_oAth.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+
 if (is_file(__DIR__.'/config.php')) {
     require_once(__DIR__.'/config.php');
 }
